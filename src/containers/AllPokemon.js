63e7ce53d4c32293977cc467d415d2379178ae14
jordles113 <jordan.t.romero@gmail.com>
2021-03-04 12:23:36 -0600

    wires up the fetch request

--- src/containers/AllPokemon.js
+++ src/containers/AllPokemon.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getPokemons } from '../services/pokemons';
 
 const AllPokemon = () => {
-  return <div>I will hold allllllll of the PokemonCard's!!</div>;
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    getPokemons().then((data) => {
+      console.log(data);
+    }, []);
+  });
+
+  return (
+    <div>
+      <div></div>I will hold allllllll of the PokemonCard's!!
+    </div>
+  );
 };
 
 export default AllPokemon;
