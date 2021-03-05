5c59b1cc620eaf3cd763afc9f6074038619f3735
jordles113 <jordan.t.romero@gmail.com>
2021-03-05 17:01:36 -0600

    gets scss wired up and starts styling pokemon cards

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 const PokemonCard = (props) => {
   return (
-    <div>
-      <h3>{props.name}</h3>
+    <div className='card'>
+      <h3 className='card__title'>{props.name}</h3>
       <img
+        className='card__img'
         src={`https://pokeres.bastionbot.org/images/pokemon/${props.id}.png`}
       />
     </div>
