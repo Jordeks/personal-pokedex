2e0ea73e0836505011e70a239aad7b36e5c6bb4b
jordles113 <jordan.t.romero@gmail.com>
2021-03-06 17:34:32 -0600

    styles the header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import AllPokemon from './containers/AllPokemon';
 const App = () => {
   return (
     <div className='App'>
-      Personal Pokedex!
-      <div>
-        <AllPokemon />
-      </div>
+      <h1 className='primary-heading'>Personal Pokedex!</h1>
+      <AllPokemon />
     </div>
   );
 };
