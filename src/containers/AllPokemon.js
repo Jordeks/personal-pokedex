a2c79654d6114d07b36450296885e12216bf0bd7
jordles113 <jordan.t.romero@gmail.com>
2021-03-09 13:53:24 -0600

    working on loading more pokemon

--- src/containers/AllPokemon.js
+++ src/containers/AllPokemon.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PokemonCard from '../components/PokemonCard';
-import { getPokemons } from '../services/pokemons';
+import { getPokemons, getMorePokemons } from '../services/pokemons';
 
 const AllPokemon = () => {
   const [pokemons, setPokemons] = useState([]);
@@ -11,12 +11,19 @@ const AllPokemon = () => {
     });
   }, []);
 
+  const morePokemon = () => {
+    getMorePokemons().then((data) => {
+      setPokemons(data);
+    });
+  };
+  console.log(pokemons);
   return (
     <div className='cards'>
       {Object.entries(pokemons)[3] &&
         Object.entries(pokemons)[3][1].map((pokemon, index) => {
           return <PokemonCard key={index} {...pokemon} id={index + 1} />;
         })}
+      <button onClick={morePokemon}>Next</button>
     </div>
   );
 };
--- src/services/pokemons.js
+++ src/services/pokemons.js
@@ -3,3 +3,9 @@ const URL = 'https://pokeapi.co/api/v2/pokemon';
 export const getPokemons = () => {
   return fetch(URL).then((response) => response.json());
 };
+
+export const getMorePokemons = () => {
+  return fetch(
+    'https://pokeapi.co/api/v2/pokemon?offset=40&limit=20'
+  ).then((response) => response.json());
+};
