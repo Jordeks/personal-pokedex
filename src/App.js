87061f218fd5b41aa03f90814f855f6099c07362
MMcClure11 <mcclme02@luther.edu>
2021-03-11 16:08:58 -0600

    Add login component and Navlink

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import AllPokemon from './containers/AllPokemon';
 import Pokemon from './components/Pokemon';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className='App'>
       <Router>
         <h1 className='primary-heading'>Personal Pokedex!</h1>
+        <NavLink to='/login' >Log in</NavLink>
         <Switch>
           <Route path='/pokemon/:id' component={Pokemon} />
+          <Route path='/login' component={Login} />
           <Route path='/' component={AllPokemon} />
         </Switch>
       </Router>
---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      I login the peeps
+    </div>
+  )
+}
+
+export default Login
