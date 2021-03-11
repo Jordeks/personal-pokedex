5f761180321e5a3875984876fa43c855b5648309
MMcClure11 <mcclme02@luther.edu>
2021-03-11 16:27:38 -0600

    Add onSubmitHandler

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,16 +5,23 @@ const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const onChange = (e) => {
+  const onChangeHandler = (e) => {
     const { name, value } = e.target
     name === "username" ? setUsername(value) : setPassword(value)
   }
-  console.log(username, password)
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+    console.log("I tried to login")
+    setUsername('')
+    setPassword('')
+  }
+
   return (
-    <form>
-      <input type="text" onChange={ onChange } name="username" value={ username } placeholder="Username" />
-      <input type="password" onChange={ onChange } name="password" value={ password } placeholder="Password" />
-      <button type="submit" >Login</button>
+    <form onSubmit={ onSubmitHandler }>
+      <input type="text" onChange={ onChangeHandler } name="username" value={ username } placeholder="Username" />
+      <input type="password" onChange={ onChangeHandler } name="password" value={ password } placeholder="Password" />
+      <input type="submit" value="Log In" />
     </form>
   )
 }
