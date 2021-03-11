80e904f6a8856f5d84e52b666d2b4bf8432949f1
MMcClure11 <mcclme02@luther.edu>
2021-03-11 16:20:11 -0600

    Add controlled form features to login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChange = (e) => {
+    const { name, value } = e.target
+    name === "username" ? setUsername(value) : setPassword(value)
+  }
+  console.log(username, password)
   return (
     <form>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
+      <input type="text" onChange={ onChange } name="username" value={ username } placeholder="Username" />
+      <input type="password" onChange={ onChange } name="password" value={ password } placeholder="Password" />
       <button type="submit" >Login</button>
     </form>
   )
