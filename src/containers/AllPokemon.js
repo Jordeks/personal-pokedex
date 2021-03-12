5c294dbd6d3b4c70348d7567c3e3c52dbd8bf941
jordles113 <jordan.t.romero@gmail.com>
2021-03-12 15:00:48 -0600

    working on making the buttons pokeballs

--- src/containers/AllPokemon.js
+++ src/containers/AllPokemon.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PokemonCard from '../components/PokemonCard';
 import { getPokemons, getMorePokemons } from '../services/pokemons';
+import Pokeball from '../images/pokeball.png';
 
 const AllPokemon = () => {
   const [pokemons, setPokemons] = useState([]);
@@ -39,8 +40,13 @@ const AllPokemon = () => {
           })}
       </div>
       <div className='footer'>
-        <button onClick={lessPokemon}>Back</button>
-        <button onClick={morePokemon}>Next</button>
+        <div className='footer__btn' onClick={lessPokemon}>
+          <img className='footer__btn-img' src={Pokeball} alt='pokeball' />
+          <span className='footer__btn-text'>Back</span>
+        </div>
+        <div className='footer__btn' onClick={morePokemon}>
+          Next
+        </div>
       </div>
     </>
   );
